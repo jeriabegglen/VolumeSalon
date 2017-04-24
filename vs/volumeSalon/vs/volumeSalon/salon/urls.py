cb233941f31dce0327531992a828c6ed7c0aa515
jeriabegglen <jeriabegglen@gmail.com>
2017-04-23 21:48:07 -0600

    Add files via upload

--- vs/volumeSalon/vs/volumeSalon/salon/urls.py
+++ vs/volumeSalon/vs/volumeSalon/salon/urls.py
@@ -1,17 +1,12 @@
 from django.conf.urls import url
-from django.contrib import admin
-from django.contrib.auth import views as auth_views
-
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-
-
+    url(r'salon/', views.product_list, name='product'),
+    url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
+        views.product_detail,
+        name='product_detail'),
+    url(r'^login', views.login, name='login'),
+    url(r'logout/', views.logout, name='logout'),
 ]
-
-    #url(r'^grade/([0-9]*)', views.saveGrade, name='grade'),
-    #url(r'^$', views.viewClients, name='viewClients'),
-    #url(r'^login/$', auth_views.login, name='login'),
-    #url(r'^logout/$', auth_views.logout, name='logout'),
-    #url(r'^admin/', admin.site.urls)
--- vs/volumeSalon/vs/volumeSalon/salon/views.py
+++ vs/volumeSalon/vs/volumeSalon/salon/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from django.contrib.auth import authenticate, login, logout
+from django.contrib import auth
+from django.utils import html
 from django.contrib.auth.decorators import login_required
-
-from .models import Client, Product, Category, BoutiqueItem, Stylist, Service
+from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password
+from django.forms import ModelForm
+from django.conf import settings
+from .models import Client, Category, Product, BoutiqueItem, Service, Stylist
 
 def index(request):
-    template = loader.get_template('salon/index.html')
-    context = {
-        'heading': 'Volume Salon POS',
-        'mission': 'Making you beautiful',
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'nbar': 'home',
+                'heading': 'Volume Salon POS',
+                'mission': 'Making you beautiful'
+            }
+    return render(request, 'salon/index.html', context)
 
 
 def viewClients(request):
@@ -24,10 +27,23 @@ def viewClients(request):
         }
     return HttpResponse(template.render(context, request))
 
+def product_list(request):
+    all_products = Product.objects.all()
+    template = loader.get_template('salon/product_list.html')
+    context = {
+        'all_products': all_products,
+        }
+    return HttpResponse(template.render(context, request))
 
+def product_detail(request):
+    all_products = Product.objects.all()
+    template = loader.get_template('salon/product_detail.html')
+    context = {
+        'all_products': all_products,
+        }
+    return HttpResponse(template.render(context, request))
 
-"""
-def login_view(request):
+def login(request):
     context = {
         'title': 'Login',
         'heading': 'Login',
@@ -67,7 +83,7 @@ def loginProcess(request):
         login_view(request)
 
 
-def logout_view(request):
+def logout(request):
     logout(request)
     context = {
         'heading': 'Successfully logged out.',
@@ -75,4 +91,3 @@ def logout_view(request):
         'title': 'Logout Successful'
     }
     return render(request, 'salon/index.html', context)
-    """
